0ad1711fa1664f25d0419860997cbf1f314a7489
Lenny HUCHER <lenny.hucher@etu.minesparis.psl.eu>
2024-06-12 10:10:08 +0200

    script to run the new openberg code updated

--- examples/example_openberg_new.py
+++ examples/example_openberg_new.py
@@ -5,13 +5,18 @@ Ice bergs (openberg)
 """
 
 from datetime import datetime, timedelta
-from opendrift.models.openberg import OpenBerg
 
-o = OpenBerg()
+# from opendrift.models.openberg import OpenBerg
+from opendrift.models.openberg_acc import IcebergDrift
 
-o.add_readers_from_list([
-        'https://thredds.met.no/thredds/dodsC/cmems/topaz6/dataset-topaz6-arc-15min-3km-be.ncml',
-        'https://pae-paha.pacioos.hawaii.edu/thredds/dodsC/ncep_global/NCEP_Global_Atmospheric_Model_best.ncd'])
+o = IcebergDrift()
+
+o.add_readers_from_list(
+    [
+        "https://thredds.met.no/thredds/dodsC/cmems/topaz6/dataset-topaz6-arc-15min-3km-be.ncml",
+        "https://pae-paha.pacioos.hawaii.edu/thredds/dodsC/ncep_global/NCEP_Global_Atmospheric_Model_best.ncd",
+    ]
+)
 
 o.seed_elements(lon=28, lat=77, time=datetime.now(), number=1000, radius=1000)
 
